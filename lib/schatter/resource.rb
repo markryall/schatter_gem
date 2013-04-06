ed661cfd9b862a9df3bb8d6709ba828238fd4248
Mark Ryall <mark@ryall.name>
2013-04-07 09:01:51 +1000

    simplified resource class

--- lib/schatter/resource.rb
+++ lib/schatter/resource.rb
@@ -52,26 +52,28 @@ class Schatter::Resource
   def nonpost url, params, verb
     params[:auth_token] = ENV['SCHATTER_AUTH_TOKEN']
     full_url = "#{url}?#{params.map{ |k,v| "#{k}=#{CGI.escape v.to_s}" }.join('&')}"
-    show_request verb, url
-    send_request do
-      HTTParty.send verb, full_url,
-        headers: {'Accept' => 'application/json'}
-    end
+    send_request verb, full_url
   end
 
   def post url, body
     body[:auth_token] = ENV['SCHATTER_AUTH_TOKEN']
-    show_request :post, url, body
-    send_request do
-      HTTParty.post url,
-        headers: {'Accept' => 'application/json', 'Content-Type' => 'application/json'},
+    send_request :post, url,
+        headers: {'Content-Type' => 'application/json'},
         body: body.to_json
-    end
   end
 
-  def send_request
+  def send_request verb, url, params={}
+    show_request verb, url, params[:body]
+    params[:headers] ||= {}
+    params[:headers]['Accept'] = 'application/json'
     start = Time.now
-    yield.tap {|response| show_response response, Time.now - start}
+    begin
+      HTTParty.send(verb, url, params).tap do |response|
+        show_response response, Time.now - start
+      end
+    rescue SocketError
+      puts 'Could not contact host - are you sure the url is correct and you have a network connection?'
+    end
   end
 
   def show_request verb, url, body=nil
@@ -82,7 +84,7 @@ class Schatter::Resource
     ].join(' ')
     puts [
       'request: ',
-      c(body.to_json, :yellow)
+      c(body, :yellow)
     ].join(' ') if body
   end
 
--- schatter.gemspec
+++ schatter.gemspec
@@ -20,6 +20,7 @@ Gem::Specification.new do |spec|
 
   spec.add_dependency 'httparty'
   spec.add_dependency 'rainbow'
+  spec.add_dependency 'shell_shock'
 
   spec.add_development_dependency 'bundler'
   spec.add_development_dependency 'rake'
